2a5f5fedd8822ce439a4851f0b45bf54d176e40a
SardorDev12 <sardoribnfarhod@gmail.com>
2024-11-28 16:40:08 +0500

    minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 import { Link, NavLink } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import "../assets/styles/components/header.scss";
-import { useAuth } from "../services/authService";
 
 const Header = () => {
-  const { user, logout } = useAuth();
   return (
     <header className=" header" id="header">
       <div className="container header-content">
@@ -24,21 +22,12 @@ const Header = () => {
               </NavLink>
             </li>
           </ul>
-          {user ? (
-            <div className="user-info">
-              <span>{user.username}</span>
-              <button className="logout-btn" onClick={logout}>
-                Log Out
-              </button>
-            </div>
-          ) : (
-            <Link to={"/login"}>
-              <button className="login-btn" type="button">
-                <FaUser />
-                KIRISH
-              </button>
-            </Link>
-          )}
+          <Link to={"/login"}>
+            <button className="login-btn" type="button">
+              <FaUser />
+              KIRISH
+            </button>
+          </Link>
         </nav>
       </div>
     </header>
